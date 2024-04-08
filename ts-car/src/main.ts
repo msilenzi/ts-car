edfa554c131e94f3cf4c9860d9dfd8b348c1eb03
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-04-08 13:36:59 -0300

    add wheelController

--- ts-car/src/main.ts
+++ ts-car/src/main.ts
@@ -1,29 +1,62 @@
+// import GamepadController from 'GamepadController/GamepadController'
+// import {
+//   XboxAxesMapper,
+//   XboxButtonMapper,
+//   xboxAxesMapper,
+//   xboxButtonMapper,
+// } from 'mappers/xboxMapper'
+
+// function handleStatusUpdated(status: {
+//   buttons: Partial<XboxButtonMapper>
+//   axes: Partial<XboxAxesMapper>
+// }) {
+//   console.log(status.buttons)
+//   console.log('LEFT_THUMBSTICK', status.axes.LEFT_THUMBSTICK ?? null)
+//   console.log('RIGHT_THUMBSTICK', status.axes.RIGHT_THUMBSTICK ?? null)
+//   console.log('\n\n')
+// }
+
+// let xboxController: GamepadController<XboxButtonMapper, XboxAxesMapper>
+
+// window.addEventListener('gamepadconnected', ({ gamepad }) => {
+//   xboxController = new GamepadController(
+//     gamepad.index,
+//     xboxButtonMapper,
+//     xboxAxesMapper,
+//     handleStatusUpdated
+//   )
+//   xboxController.start()
+// })
+
+//
+//
+//
+
 import GamepadController from 'GamepadController/GamepadController'
 import {
-  XboxAxesMapper,
-  XboxButtonMapper,
-  xboxAxesMapper,
-  xboxButtonMapper,
-} from 'mappers/xboxMapper'
+  WheelAxesMapper,
+  WheelButtonMapper,
+  wheelAxesMapper,
+  wheelButtonMapper,
+} from 'mappers/wheelMapper'
+
+let wheelController: GamepadController<WheelButtonMapper, WheelAxesMapper>
 
 function handleStatusUpdated(status: {
-  buttons: Partial<XboxButtonMapper>
-  axes: Partial<XboxAxesMapper>
+  buttons: Partial<WheelButtonMapper>
+  axes: Partial<WheelAxesMapper>
 }) {
   console.log(status.buttons)
-  console.log('LEFT_THUMBSTICK', status.axes.LEFT_THUMBSTICK ?? null)
-  console.log('RIGHT_THUMBSTICK', status.axes.RIGHT_THUMBSTICK ?? null)
-  console.log('\n\n')
+  console.log(status.axes.direccion)
 }
 
-let xboxController: GamepadController<XboxButtonMapper, XboxAxesMapper>
-
 window.addEventListener('gamepadconnected', ({ gamepad }) => {
-  xboxController = new GamepadController(
+  wheelController = new GamepadController(
     gamepad.index,
-    xboxButtonMapper,
-    xboxAxesMapper,
+    wheelButtonMapper,
+    wheelAxesMapper,
     handleStatusUpdated
   )
-  xboxController.start()
+
+  wheelController.start()
 })
---
+++ ts-car/src/mappers/wheelMapper.ts
@@ -0,0 +1,11 @@
+export const wheelButtonMapper = {
+  adelante: 5,
+  atras: 4,
+}
+
+export const wheelAxesMapper = {
+  direccion: {x: 0},
+}
+
+export type WheelButtonMapper = typeof wheelButtonMapper
+export type WheelAxesMapper = typeof wheelAxesMapper
