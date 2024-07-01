c38b0e966be636f1080e54289c3efce53b385adf
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-07-01 19:38:55 -0300

    add ui/form.ts

--- ts-car/src/main.ts
+++ ts-car/src/main.ts
@@ -1,13 +1,30 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import AbstractCarController from 'CarController/AbstractCarController'
+import AbstractCarController, {
+  CarMapper,
+} from 'CarController/AbstractCarController'
 import AdvancedCarController from 'CarController/AdvancedCarController'
 import BasicCarController from 'CarController/BasicCarController'
 
 import { wheelCarMapper, xboxCarMapper } from 'CarController/mappers'
+import form from './ui/form.ts'
 
-const TYPES = [
+export type TypeOption = {
+  displayName: string
+  mapper: CarMapper
+}
+
+export type DriverOption = {
+  displayName: string
+  Driver: new (
+    gamepadIndex: number,
+    carMapper: CarMapper,
+    CAR_URL: string
+  ) => AbstractCarController
+}
+
+const TYPES: TypeOption[] = [
   {
     displayName: 'Mando de Xbox',
     mapper: xboxCarMapper,
@@ -18,112 +35,45 @@ const TYPES = [
   },
 ]
 
-const DRIVERS = [
+const DRIVERS: DriverOption[] = [
   {
     displayName: 'Controlador básico',
-    driver: BasicCarController,
+    Driver: BasicCarController,
   },
   {
     displayName: 'Controlador avanzado',
-    driver: AdvancedCarController,
+    Driver: AdvancedCarController,
   },
 ]
 
-const $form = document.querySelector<HTMLFormElement>('form')!
-const $inputIpAddr = document.querySelector<HTMLInputElement>('#ipAddress')!
-const $selectController =
-  document.querySelector<HTMLSelectElement>('#controller')!
-const $selectType = document.querySelector<HTMLSelectElement>('#type')!
-const $selectDriver = document.querySelector<HTMLSelectElement>('#driver')!
-
-const $status = document.querySelector<HTMLSpanElement>('#connectionStatus')!
+const $status: HTMLSpanElement = document.querySelector('#connectionStatus')!
 
 let carController: AbstractCarController | null = null
 
-function loadControllerOptions() {
-  const gamepads = navigator.getGamepads()
-
-  $selectController.innerHTML = ''
-
-  if (gamepads.every((gamepad) => gamepad === null)) {
-    const $option = document.createElement('option')
-    $option.disabled = true
-    $option.value = ''
-    $option.innerText = 'No hay mandos disponibles'
-    $option.selected = true
-    $selectController.appendChild($option)
-  } else {
-    gamepads.forEach((g) => {
-      if (g === null) return
-      const $option = document.createElement('option')
-      $option.value = g.index.toString()
-      $option.innerText = g.id
-      $selectController.appendChild($option)
-    })
-  }
-}
-
-function loadTypeOptions() {
-  $selectType.innerHTML = ''
-  TYPES.forEach((type, index) => {
-    const $option = document.createElement('option')
-    $option.value = index.toString()
-    $option.innerText = type.displayName
-    $selectType.appendChild($option)
-  })
-}
-
-function loadDriversOptions() {
-  $selectDriver.innerHTML = ''
-  DRIVERS.forEach((driver, index) => {
-    const $option = document.createElement('option')
-    $option.value = index.toString()
-    $option.innerText = driver.displayName
-    $selectDriver.appendChild($option)
-  })
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-  loadControllerOptions()
-  loadTypeOptions()
-  loadDriversOptions()
+  form.loadControllerOptions()
+  form.loadTypeOptions(TYPES)
+  form.loadDriversOptions(DRIVERS)
 
   $status.innerText = 'desconectado'
   $status.className = 'text-danger'
 })
 
-$form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  $form.classList.add('was-validated')
-  if (!$form.checkValidity()) {
-    e.stopPropagation()
-    return;
-  }
-
-  // Detiene el auto si ya estaba conectado
-  if (carController !== null) carController.stop()
-
-  const ipAddr = $inputIpAddr.value
-  const controller = parseInt($selectController.value, 10)
-  const type = parseInt($selectType.value, 10)
-  const driver = parseInt($selectDriver.value, 10)
-
-  carController = new DRIVERS[driver].driver(
-    controller,
-    TYPES[type].mapper,
-    ipAddr
+form.bindOnFinish(({ ipAddress, controllerIndex, typeIndex, driverIndex }) => {
+  carController = new DRIVERS[driverIndex].Driver(
+    controllerIndex,
+    TYPES[typeIndex].mapper,
+    ipAddress
   )
-
   carController.start()
 })
 
 window.addEventListener('gamepadconnected', () => {
-  loadControllerOptions()
+  form.loadControllerOptions()
 })
 
 window.addEventListener('gamepaddisconnected', () => {
-  loadControllerOptions()
+  form.loadControllerOptions()
   carController?.stop()
   carController = null
 })
---
+++ ts-car/src/ui/form.ts
@@ -0,0 +1,117 @@
+import { DriverOption, TypeOption } from '../main.ts'
+
+export type FormData = {
+  ipAddress: string
+  controllerIndex: number
+  typeIndex: number
+  driverIndex: number
+}
+
+const $form: HTMLFormElement = document.querySelector('form')!
+const $inputIpAddr: HTMLInputElement = document.querySelector('#ipAddress')!
+const $selectController: HTMLSelectElement =
+  document.querySelector<HTMLSelectElement>('#controller')!
+const $selectType: HTMLSelectElement = document.querySelector('#type')!
+const $selectDriver: HTMLSelectElement = document.querySelector('#driver')!
+
+function loadControllerOptions() {
+  const gamepads = navigator.getGamepads()
+
+  $selectController.innerHTML = ''
+
+  if (gamepads.every((gamepad) => gamepad === null)) {
+    const $option = document.createElement('option')
+    $option.disabled = true
+    $option.value = ''
+    $option.innerText = 'No hay mandos disponibles'
+    $option.selected = true
+    $selectController.appendChild($option)
+  } else {
+    gamepads.forEach((g) => {
+      if (g === null) return
+      const $option = document.createElement('option')
+      $option.value = g.index.toString()
+      $option.innerText = g.id
+      $selectController.appendChild($option)
+    })
+  }
+}
+
+function loadTypeOptions(types: TypeOption[]) {
+  $selectType.innerHTML = ''
+  types.forEach((type, index) => {
+    const $option = document.createElement('option')
+    $option.value = index.toString()
+    $option.innerText = type.displayName
+    $selectType.appendChild($option)
+  })
+}
+
+function loadDriversOptions(drivers: DriverOption[]) {
+  $selectDriver.innerHTML = ''
+  drivers.forEach((driver, index) => {
+    const $option = document.createElement('option')
+    $option.value = index.toString()
+    $option.innerText = driver.displayName
+    $selectDriver.appendChild($option)
+  })
+}
+
+function bindOnFinish(handler: (values: FormData) => void) {
+  $form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    // Validar formulario
+    const formValidiy = $form.checkValidity()
+    $form.classList.add('was-validated')
+
+    // Si los inputs son inválidos terminar
+    if (!formValidiy) {
+      e.stopPropagation()
+      return
+    }
+
+    _validateIpAddress()
+
+    handler(_getFormValues())
+  })
+}
+
+function _getFormValues(): FormData {
+  return {
+    ipAddress: $inputIpAddr.value,
+    controllerIndex: parseInt($selectController.value),
+    typeIndex: parseInt($selectType.value),
+    driverIndex: parseInt($selectDriver.value),
+  }
+}
+
+async function _validateIpAddress(): Promise<boolean> {
+  // Si la URL termina con una / se elimina
+  if ($inputIpAddr.value.endsWith('/')) {
+    $inputIpAddr.value = $inputIpAddr.value.slice(0, -1)
+  }
+
+  // Envía un fetch /parar a la URL ingresada. Si en 1 segundo no responde
+  // cancela la petición y devuelve false:
+
+  const controller = new AbortController()
+  const timeoutId = setTimeout(() => controller.abort(), 1000)
+
+  try {
+    const resp = await fetch(`${$inputIpAddr.value}/parar`, {
+      signal: controller.signal,
+    })
+    clearTimeout(timeoutId)
+    return resp.ok
+  } catch (error) {
+    return false
+  }
+}
+
+export default {
+  loadControllerOptions,
+  loadTypeOptions,
+  loadDriversOptions,
+  bindOnFinish,
+}
