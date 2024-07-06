7dd87757aea9dd0be68fc30867b8145820cd728a
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-07-05 21:24:14 -0300

    add viteSingleFile plugin

--- ts-car/vite.config.ts
+++ ts-car/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), viteSingleFile()],
 })
