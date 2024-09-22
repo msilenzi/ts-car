220e11177127a329c5979234fc150a27f5412980
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-22 15:13:54 -0300

    update GamepadController

--- ts-car/src/GamepadController/GamepadController.ts
+++ ts-car/src/GamepadController/GamepadController.ts
@@ -1,74 +1,68 @@
-import GamepadAxes, { AxesMapper } from './GamepadAxes'
-import GamepadButtons, { ButtonMapper } from './GamepadButtons'
+import AbstractGamepadInput, {
+  AllowedGamepadInputValues,
+} from './GamepadInput/AbstractGamepadInput.ts'
 
-export default abstract class GamepadController<
-  T extends ButtonMapper,
-  U extends AxesMapper,
-> {
-  private readonly pollIntervalMs: number
-  private interval: number | null
-  private buttons: GamepadButtons<T>
-  private axes: GamepadAxes<U>
+export type GamepadMapper = Record<
+  string,
+  AbstractGamepadInput<AllowedGamepadInputValues>
+>
+
+export type GamepadStatus<T extends GamepadMapper> = {
+  [K in keyof T]: ReturnType<T[K]['getStatus']>
+}
+
+export default abstract class GamepadController<T extends GamepadMapper> {
+  private readonly POLL_INTERVAL_TIME_MS: number
+  private pollInterval: number | null
+  private readonly mapper: T
   private readonly gamepadIndex: number
 
-  protected constructor(
-    gamepadIndex: number,
-    gamepadButtons: GamepadButtons<T>,
-    gamepadAxes: GamepadAxes<U>
-  ) {
-    this.pollIntervalMs = 50
-    this.interval = null
-
+  protected constructor(gamepadIndex: number, mapper: T) {
     this.gamepadIndex = gamepadIndex
-    this.buttons = gamepadButtons
-    this.axes = gamepadAxes
+    this.mapper = mapper
+
+    this.POLL_INTERVAL_TIME_MS = 50
+    this.pollInterval = null
   }
 
   public start(): void {
-    if (this.interval !== null) {
-      throw new Error('Gamepad already started')
+    if (this.pollInterval !== null) {
+      throw new Error('Gamepad polling already started')
     }
 
-    this.interval = setInterval(() => {
-      const gamepad = navigator.getGamepads()[this.gamepadIndex]!
+    this.pollInterval = setInterval(() => {
+      const gp = navigator.getGamepads()[this.gamepadIndex]
+      if (gp === null) throw new Error('Invalid gamepad')
 
-      const updatedStatus = {
-        buttons: this.buttons.updateStatus(
-          gamepad.buttons.map(({ value }) => value)
-        ),
-        axes: this.axes.updateStatus([...gamepad.axes]),
-      }
+      const updatedStatus: Partial<GamepadStatus<T>> = Object.entries(
+        this.mapper
+      ).reduce((acc, [key, input]) => {
+        if (!input.updateStatus(gp)) return acc
+        return { ...acc, [key]: input.getStatus() }
+      }, {})
 
-      if (
-        Object.keys(updatedStatus.buttons).length !== 0 ||
-        Object.keys(updatedStatus.axes).length !== 0
-      ) {
+      if (Object.keys(updatedStatus).length !== 0) {
         this.handleStatusUpdated(updatedStatus)
       }
-    }, this.pollIntervalMs)
+    }, this.POLL_INTERVAL_TIME_MS)
   }
 
   public stop(): void {
-    if (this.interval === null) {
-      throw new Error('Gamepad already stopped')
+    if (this.pollInterval === null) {
+      throw new Error('Gamepad polling already stopped')
     }
 
-    clearInterval(this.interval)
-    this.buttons.stop()
-    this.axes.stop()
+    clearInterval(this.pollInterval)
+    Object.values(this.mapper).forEach((input) => input.initializeStatus())
     this.handleStatusUpdated(this.getStatus())
-    this.interval = null
+    this.pollInterval = null
   }
 
-  public getStatus(): { buttons: T; axes: U } {
-    return {
-      buttons: this.buttons.getStatus(),
-      axes: this.axes.getStatus(),
-    }
+  public getStatus(): GamepadStatus<T> {
+    return Object.entries(this.mapper).reduce((acc, [key, input]) => {
+      return { ...acc, [key]: input.getStatus() }
+    }, {} as GamepadStatus<T>)
   }
 
-  public abstract handleStatusUpdated(status: {
-    buttons: Partial<T>
-    axes: Partial<U>
-  }): void
+  public abstract handleStatusUpdated(status: Partial<GamepadStatus<T>>): void
 }
