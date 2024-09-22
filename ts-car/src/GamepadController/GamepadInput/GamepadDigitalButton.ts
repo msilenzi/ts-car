cac57f1dcfed9323cabc6baae8e9fb90165b5ebb
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-21 23:44:49 -0300

    add constructor to GamepadDigitalButton

--- ts-car/src/GamepadController/GamepadInput/GamepadDigitalButton.ts
+++ ts-car/src/GamepadController/GamepadInput/GamepadDigitalButton.ts
@@ -1,6 +1,10 @@
 import AbstractGamepadInput from './AbstractGamepadInput.ts'
 
 export default class GamepadDigitalButton extends AbstractGamepadInput {
+  public constructor(index: number) {
+    super(index)
+  }
+
   public hasBeenUpdated(gamepad: Gamepad): boolean {
     return this.getStatus() !== this.getCurrentValue(gamepad)
   }
